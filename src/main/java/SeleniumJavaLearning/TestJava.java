625938444c18f3f9765c605f4f4a689a62917e7a
GowthamShanthi <gowtham.shanthi@gmail.com>
2024-06-23 12:21:22 +0530

    Adding Test file

---
+++ src/main/java/SeleniumJavaLearning/TestJava.java
@@ -0,0 +1,12 @@
+package SeleniumJavaLearning;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Namachivaya");
+	}
+
+}
